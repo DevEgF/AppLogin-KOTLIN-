da2637ebd79ee1ceddeeba978ca4772959c45b7b
Egito <egitofilho4@gmail.com>
2022-05-15 14:53:58 -0300

    primeiro app em Kotlin

---
+++ app/src/main/java/filho/egito/myapplogin/MainActivity.kt
@@ -0,0 +1,11 @@
+package filho.egito.myapplogin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+         }
+}
---
+++ app/src/main/java/filho/egito/myapplogin/SingUpActivity.kt
@@ -0,0 +1,51 @@
+package filho.egito.myapplogin
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import filho.egito.myapplogin.databinding.ActivitySingUpBinding
+
+class SingUpActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivitySingUpBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivitySingUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        binding.button.setOnClickListener{
+            val email = binding.emailEt.text.toString()
+            val senha = binding.passET.text.toString()
+            val confirma = binding.confirmPassEt.text.toString()
+
+            if (email.isNotEmpty() && senha.isNotEmpty() && confirma.isNotEmpty()) {
+                if (senha == confirma) {
+
+                    firebaseAuth.createUserWithEmailAndPassword(email, senha).addOnCompleteListener {
+
+                        if (it.isSuccessful) {
+                            val intent = Intent(this, SingInActivity::class.java)
+                            startActivity(intent)
+                        } else {
+                            Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+
+                        }
+                    }
+                } else {
+                    Toast.makeText(this, "Senha incorreta", Toast.LENGTH_SHORT).show()
+                }
+            } else {
+                Toast.makeText(this, "Campos vazios não são permitidos !!!", Toast.LENGTH_SHORT).show()
+
+            }
+        }
+    }
+}
+
